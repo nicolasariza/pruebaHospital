c96120fe932b57b31e78bc0d04e8bd45a0c67428
Nicolas Ariza <near5@outlook.com>
2018-10-01 00:41:06 -0500

    cambios en login
    edicion del layout app.blade para modificar el navbar, y el login.blade para poner la imagen del hospital

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" type"text/css" href="/css/app.css">
         <title>Laravel</title>
 
         <!-- Fonts -->
@@ -77,20 +77,9 @@
                 </div>
             @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
         </div>
     </body>
 </html>
+<?php
+
+?>
